1c6efd05c63932d5bce220966db68fe428a50700
guudewie <philippthiessen12@gmail.com>
2024-08-08 17:30:54 +0200

    remove connection string conditional

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,5 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production"
-      ? process.env.DATABASE_URL
-      : process.env.DATABASE_PUBLIC_URL,
+  connectionString: process.env.DATABASE_PUBLIC_URL,
 });
