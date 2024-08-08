62e521549ee0cedffd33f30e233bbd8b388f2526
guudewie <philippthiessen12@gmail.com>
2024-08-08 11:34:29 +0200

    revert changes

--- app.js
+++ app.js
@@ -25,9 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 
-console.log(process.env);
-const PORT =
-  process.env.PORT || process.env.PG_PORT || process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 
 // catch 404 and forward to error handler
